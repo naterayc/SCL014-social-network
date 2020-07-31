9be55ae4942e59696f5e7b6a3c569479392f4548
yndira <yndinatera10@gmail.com>
2020-07-31 11:42:00 -0400

    autenticacion con google

--- main.js
+++ main.js
@@ -18,3 +18,37 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+
+document.getElementById('signInGoogle').addEventListener('click', () => {
+    authGoogle();
+});
+
+function authGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    autenthication(provider);
+};
+
+function autenthication(provider) {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        console.log(token);
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(errorCode);
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // The email of the user's account used.
+        var email = error.email;
+        console.log(email);
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(credential);
+        // ...
+    });
+}
