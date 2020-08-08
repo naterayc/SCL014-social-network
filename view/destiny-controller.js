1fee556bd5aa981443bbaf8d277a035222bd8426
yndira <yndinatera10@gmail.com>
2020-08-07 22:51:49 -0400

    estilos pantalla perfil de usuario

--- view/destiny-controller.js
+++ view/destiny-controller.js
@@ -1,6 +1,5 @@
-import {
-    changeRoute, pushState
-} from '../view-controller/router.js'
+import { pushState } from '../view-controller/router.js'
+import { signOut } from '../lib/firebase/firebase-auth.js';
 import { destinyContent } from './destiny-view.js';
 
 export const destinyView = () => {
@@ -25,5 +24,18 @@ export const destinyView = () => {
     const goProfile = document.getElementById('profile');
     goProfile.addEventListener('click', () => {
         pushState('#/profile');
+    });
+
+    const menu = document.getElementById('show-signout');
+    const signOutBar = document.getElementById('sign-out');
+    menu.addEventListener('click', () => {
+        signOutBar.style.display = 'block';
+    });
+
+    signOutBar.addEventListener('click', () => {
+        signOut();
+        const body = document.getElementById('body');
+        body.style.background = 'linear-gradient(180deg, #FFFFFF 6.25%, #C5E0EF 35.94%, #2979FF 76.04%)';
+        body.style.backgroundAttachment = ' fixed';
     });
 }
