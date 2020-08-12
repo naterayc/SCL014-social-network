384c485d7ebe3de651f501c757583df6997f3c30
yndira <yndinatera10@gmail.com>
2020-08-11 21:58:14 -0400

    configuracion de estilos, replicamiento de maquetacion

---
+++ view-controller/profile-controller.js
@@ -0,0 +1,17 @@
+import { pushState } from './router.js'
+import { profileContent } from '../view/profile-view.js';
+
+export const profileView = () => {
+
+    document.getElementById('body').style.background = 'transparent';
+   
+    const profileWall = document.createElement('div');
+    profileWall.setAttribute('id', 'contenedor');
+    profileWall.innerHTML = profileContent;
+    document.getElementById('containerViews').appendChild(profileWall);
+    
+    const goHome = document.getElementById('home');
+    goHome.addEventListener('click', () => {
+        pushState('#/wall')
+    }); 
+}
--- view/mypost-content-view.js
+++ view/mypost-content-view.js
@@ -1,18 +1,33 @@
 const mypostPlantilla = `
 <div id="mypost-note">
+    
     <div>
         <i class="fas fa-ellipsis-h icon"></i>
     </div>
 
     <div>
-        <img src="img/islandia.jpg"  alt="Foto post" class="img-mypost">
+        <img src="img/snow.jpg"  alt="Foto post" class="img-mypost">
     </div>
 
     <div>
         <span id="commentsNumber" class="mylike-comment">12</span><i class="far fa-comment-alt icon"></i>
         <span id="likesNumber" class="mylike-comment">30</span><i class="far fa-heart icon"></i>
-    </div>
+    </div>       
+</div>
+<div id="mypost-note">
     
+    <div>
+        <i class="fas fa-ellipsis-h icon"></i>
+    </div>
+
+    <div>
+        <img src="img/city.jpg"  alt="Foto post" class="img-mypost">
+    </div>
+
+    <div>
+        <span id="commentsNumber" class="mylike-comment">12</span><i class="far fa-comment-alt icon"></i>
+        <span id="likesNumber" class="mylike-comment">30</span><i class="far fa-heart icon"></i>
+    </div>       
 </div>
 `;
 export {mypostPlantilla};
