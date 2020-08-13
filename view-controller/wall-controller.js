d792b430f71398871d4dc19d84ce80f71fbb4934
yndira <yndinatera10@gmail.com>
2020-08-12 21:59:37 -0400

    se agrega mensaje para el escenario de que no hayan post, se coloca foto del usuario en el post

--- view-controller/wall-controller.js
+++ view-controller/wall-controller.js
@@ -1,14 +1,15 @@
 import { pushState } from './router.js'
 import { wallContent } from '../view/wall-view.js';
 import { signOut } from '../lib/firebase/firebase-auth.js';
-import { 
+import {
     savePublish,
     obtenerPublish,
     deletePublish,
     getDoc,
     updateDoc
- } from '../lib/firebase/firebase-firestore.js';
+} from '../lib/firebase/firebase-firestore.js';
 import { postPlantilla } from "../view/post-content-view.js";
+import { postEmptyPlantilla } from "../view/post-empty-view.js"
 
 export const wallView = () => {
 
@@ -49,11 +50,14 @@ export const wallView = () => {
         const postText = document.querySelector('#post-title').value;
         const postImg = photo;
         const date = new Date().toLocaleString();
+        const user = JSON.parse(localStorage.getItem('user'));        
         savePublish({
             postText: postText,
             postImg: postImg,
             date: date,
             likes: [],
+            userUid: user.uid,
+            userPhoto: user.photo
         }).then(() => {
             getPublishPrint();
         });
@@ -73,7 +77,7 @@ export const wallView = () => {
     signOutBar.addEventListener('click', () => {
         signOut();
         const body = document.querySelector('#body');
-        const screen = window.matchMedia('(min-width: 700px)');        
+        const screen = window.matchMedia('(min-width: 700px)');
         const showWallDesktop = () => {
             const myMedia = (screenSize) => {
                 if (screenSize.matches) { // If media query matches
@@ -81,9 +85,9 @@ export const wallView = () => {
                     body.style.background = 'trasparent';
                     body.style.backgroundImage = 'url("./img/backimg.jpg")';
                     body.style.backgroundAttachment = 'fixed';
-                    body.style.margin = '0';        
-                    body.style.backgroundSize= 'cover';               
-                    
+                    body.style.margin = '0';
+                    body.style.backgroundSize = 'cover';
+
                 } else {
                     /*console.log('screen menor 700px');*/
                     body.style.background = 'linear-gradient(180deg, #FFFFFF 6.25%, #C5E0EF 35.94%, #2979FF 76.04%)';
@@ -107,59 +111,63 @@ export const wallView = () => {
         pushState('#/profile');
     });
 
-    
+
     const getPublishPrint = () => {
         container.innerHTML = '';
-        obtenerPublish().then((arrayPublish) => {
-            renderPublish(arrayPublish, container);
-            const editor = container.querySelectorAll('.show-options');
-            editor.forEach(icon => {
-                const parentDiv = icon.parentElement.parentElement;
-                icon.addEventListener('click', () => {
-                    parentDiv.querySelector('.edit-post').style.display = "flex";
-                });
-                parentDiv.querySelector('.delete-option').addEventListener('click', () => {
-                    deletePublish(parentDiv.dataset.id)
-                        .then(() => {
-                            getPublishPrint();
-                        })
-                });
-                // evento para dar like a los post
-                parentDiv.querySelector('[data-id="likesNumber"]').addEventListener('click', () => {
-                    console.log('likes');
-                    getDoc(parentDiv.dataset.id)
-                        .then((doc) => {                            
-                            const user = JSON.parse(localStorage.getItem('user'));                            
-                            const docData = doc.data();
-                            //recorre la data y accede al arreglo de likes 
-                            const dataLikes = docData.likes
-                            // busca si exite id en arreglo
-                            const likeIndex = dataLikes.indexOf(user.uid);                            
-                            // si no existe, agregalo
-                            if(likeIndex ===-1){
-                                dataLikes.push(user.uid);
-                            }
-                            // si existe, elimina el id del arreglo
-                            else{
-                                dataLikes.splice(likeIndex,1);
-                            }                            
-                            updateDoc(parentDiv.dataset.id, {
-                                likes: dataLikes
-                            }).then(() => {
-                                //buscar elemento a actualizar del html
-                                const likesUpdate = parentDiv.querySelector('[data-id="likesNumber"]');
-                                //ir a firebase y buscar valor de likes
-                                getDoc(parentDiv.dataset.id)
-                                    .then((doc) => {
-                                        const dataDoc = doc.data();
-                                        //actualizar el valor retornado al html                           
-                                        likesUpdate.innerHTML = dataDoc.likes.length;
-                                    });
+        obtenerPublish().then((arrayPublish) => {            
+            if (arrayPublish.empty === true) {
+                renderEmptyPublish(container);
+            } else {
+                renderPublish(arrayPublish, container);
+                const editor = container.querySelectorAll('.show-options');
+                editor.forEach(icon => {
+                    const parentDiv = icon.parentElement.parentElement;
+                    icon.addEventListener('click', () => {
+                        parentDiv.querySelector('.edit-post').style.display = "flex";
+                    });
+                    parentDiv.querySelector('.delete-option').addEventListener('click', () => {
+                        deletePublish(parentDiv.dataset.id)
+                            .then(() => {
+                                getPublishPrint();
+                            })
+                    });
+                    // evento para dar like a los post
+                    parentDiv.querySelector('[data-id="likesNumber"]').addEventListener('click', () => {
+                        console.log('likes');
+                        getDoc(parentDiv.dataset.id)
+                            .then((doc) => {
+                                const user = JSON.parse(localStorage.getItem('user'));
+                                const docData = doc.data();
+                                //recorre la data y accede al arreglo de likes 
+                                const dataLikes = docData.likes
+                                // busca si exite id en arreglo
+                                const likeIndex = dataLikes.indexOf(user.uid);
+                                // si no existe, agregalo
+                                if (likeIndex === -1) {
+                                    dataLikes.push(user.uid);
+                                }
+                                // si existe, elimina el id del arreglo
+                                else {
+                                    dataLikes.splice(likeIndex, 1);
+                                }
+                                updateDoc(parentDiv.dataset.id, {
+                                    likes: dataLikes
+                                }).then(() => {
+                                    //buscar elemento a actualizar del html
+                                    const likesUpdate = parentDiv.querySelector('[data-id="likesNumber"]');
+                                    //ir a firebase y buscar valor de likes
+                                    getDoc(parentDiv.dataset.id)
+                                        .then((doc) => {
+                                            const dataDoc = doc.data();
+                                            //actualizar el valor retornado al html                           
+                                            likesUpdate.innerHTML = dataDoc.likes.length;
+                                        });
+                                });
                             });
-                        });
-                });
-            });
+                    });
 
+                });
+            }
         });
     };
 
@@ -168,13 +176,13 @@ export const wallView = () => {
 //pinta los post en pantalla
 const renderPublish = (arrayPublish, container) => {
     arrayPublish.forEach(doc => {
-        const data = doc.data();
+        const data = doc.data();        
         const postRender = postPlantilla(
             doc.id,
             data.postImg,
             0,
             data.likes.length,
-            'https://avatars2.githubusercontent.com/u/66418488?s=460&u=426e67c2f5f2fb4dc243355a6c1f2da9841a0370&v=4',
+            data.userPhoto ? data.userPhoto : 'img/viajera3.jpg',
             data.postText,
             data.date
         );
@@ -182,4 +190,9 @@ const renderPublish = (arrayPublish, container) => {
         divPost.innerHTML = postRender;
         container.appendChild(divPost);
     });
+};
+const renderEmptyPublish = (container) => {
+    const divPost = document.createElement('div');
+    divPost.innerHTML = postEmptyPlantilla;
+    container.appendChild(divPost);
 };
