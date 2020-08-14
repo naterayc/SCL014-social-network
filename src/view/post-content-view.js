d6ee71efc6646933deacb489d89928770c514732
yndira <yndinatera10@gmail.com>
2020-08-14 13:16:37 -0400

    estilos a los textos de la publicacion, estilos para editar publicacion, se agrega nombre de usuario a la publicacion

--- src/view/post-content-view.js
+++ src/view/post-content-view.js
@@ -1,5 +1,5 @@
-const postPlantilla = (docId, image, comments, likes, photo, text, date) =>  `
-<div id="post-note" data-id="${ docId }">
+const postPlantilla = (docId, image, comments, likes, photo, text, name, date) => `
+<div id="post-note" data-id="${ docId}">
         <div>
             <i class="fas fa-ellipsis-h icon show-options"></i>
          </div>
@@ -9,25 +9,25 @@ const postPlantilla = (docId, image, comments, likes, photo, text, date) =>  `
         </div>
         <!-- <p>Madrid</p> -->
         <div> 
-            <img id= "image-load" src="${ image }"  alt="Foto post" class="img-cuadrada">        
+            <img id= "image-load" src="${ image}"  alt="Foto post" class="img-cuadrada">        
         </div>
         
         <div>              
-            <span id="commentsNumber" class="like-comment">${ comments }</span><i class="far fa-comment-alt icon"></i>
-            <span data-id="likesNumber" class="like-comment">${ likes }</span><i class="far fa-heart icon"></i>
+            <span id="commentsNumber" class="like-comment">${ comments}</span><i class="far fa-comment-alt icon"></i>
+            <span data-id="likesNumber" class="like-comment">${ likes}</span><i class="far fa-heart icon"></i>
         </div>
         <div>
-            <img src="${ photo }" id="picture-perfil-post" alt="Foto perfil">  
-            <h4 data-id="text-post-edited">${ text }</h4>
+            <img src="${ photo}" id="picture-perfil-post" alt="Foto perfil">  
+            <p data-id="text-post-edited" class="post-text">${ text}</p>
             <div data-id="div-edit-post" class="hide">
-                <input data-id="post-text-edit" class="inputpublic" type="text" value="${ text }"/>
-                <button data-id="btn-save-input">GUARDAR</button>
-            </div>
-            <div>
-                <p>${ date }</p>
+                <input data-id="post-text-edit" class="edit-input" type="text" value="${ text}"/>
+                <button data-id="btn-save-input" class="save-btn"><i class="fas fa-check"></i></button>
             </div>
         </div>
-        
+            <div>
+                <p class="date"> ${ name }</p>
+                <p class="date">${ date }</p>
+            </div>             
 </div>
 `;
-export {postPlantilla};
+export { postPlantilla };
