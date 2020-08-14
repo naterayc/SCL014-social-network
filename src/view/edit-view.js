61b3fdc08ff84de9d9186fa47c27c85cab4ae7af
yndira <yndinatera10@gmail.com>
2020-08-14 19:07:27 -0400

    se agrega confirmacion antes de eliminar el post, se modifica input para editar el post (textarea)

--- src/view/edit-view.js
+++ src/view/edit-view.js
@@ -6,4 +6,13 @@ export const editOptions = `
         <span data-id="edit-post"><i class="far fa-edit icon"></i>Editar</span>
         <span><i class="fas fa-trash-alt icon delete-option"></i>Eliminar</span>
     </div>
- `;
+ 
+ <div id="detele-modal" class="modal hide">
+    <section class="modal-content">
+        <h3> ¿Realmente quieres eliminar esta publicación? </h3>
+        <div style="display:flex">
+        <button id="delete" class="delete-btn"> <!-- <i class="fas fa-trash-alt"> </i> --> Eliminar</button>
+        <button id="close-delete" class="delete-btn"><!-- <i class="fas fa-window-close"> </i> --> Cancelar</button>
+        <div>
+    </section>
+ </div>   `;
